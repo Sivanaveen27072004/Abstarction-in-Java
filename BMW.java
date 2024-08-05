2827ab9fc713c081bb1374ffc8ef7a4c56edf1c4
Sivnaveen L. <sivanaveenecemec@gmail.com>
2024-08-05 14:21:13 +0530

    BMW.java

---
+++ BMW.java
@@ -0,0 +1,28 @@
+
+public class BMW implements Car {
+
+	@Override
+	public void start() {
+		System.out.println("BMW Car started v10 m3");
+		
+	}
+
+	@Override
+	public void accelerate() {
+		System.out.println("BMW started in 8 sec");
+		
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("The Petrol Consumption is 100ml");
+		
+	}
+
+	@Override
+	public void seatbelt() {
+		System.out.println("6 Seatbelts");
+		
+	}
+
+}
